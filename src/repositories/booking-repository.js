6faf9e59d7f22d7c0d0c8ea8c5a7d3aa71efe3e7
Sachins0 <sachins.ssingh0@gmail.com>
2024-10-11 12:02:44 +0530

    Added cancel booking logic with CRON

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -2,6 +2,9 @@ const { StatusCodes } = require('http-status-codes');
 
 const { Booking } = require('../models');
 const CrudRepository = require('./crud-repository');
+const {Enums}= require('../utils/common');
+const { Op } = require('sequelize');
+const {BOOKED, CANCELLED}= Enums.BOOKING_STATUS;
 
 class BookingRepository extends CrudRepository {
     constructor() {
@@ -11,7 +14,51 @@ class BookingRepository extends CrudRepository {
     async createBooking(data, transaction) {
         const response = await Booking.create(data, {transaction: transaction});
         return response;
-    } 
+    }
+    
+    async get(data, transaction){
+        const response= await Booking.findByPk(data, {transaction: transaction});
+        if(!response){
+            throw new AppError('Not able to find the resource',StatusCodes.NOT_FOUND);
+        }
+        return response;
+    }
+
+    async update(id, data, transaction){
+        const response= Booking.update(data, {
+            where: {
+                id: id
+            }
+        },{transaction: transaction});
+        return response;
+    }
+
+    async cancelOldBookings(timestamp){
+        const response= await Booking.update({status: CANCELLED},{
+            where:{
+                [Op.and]: [
+                    {
+                        createdAt: {
+                            [Op.lt]: timestamp
+                        }
+                    },
+                    {
+                        status: {
+                            [Op.ne]: BOOKED
+                        }
+                    },
+                    {
+                        status: {
+                            [Op.ne]: CANCELLED
+                        }
+                    }
+                ]
+            }
+        });
+        return response;
+    }
 }
+
+
 
 module.exports = BookingRepository;
--- src/routes/v1/booking-routes.js
+++ src/routes/v1/booking-routes.js
@@ -10,5 +10,7 @@ router.post(
     BookingController.createBooking
 )
 
+router.post('/payments',BookingController.makePayment);
+
 
 module.exports = router;
